50aa4df901b0950c66505b09d223db3b1c35d008
Francisco Puga <fpuga@cartolab.es>
2011-08-07 13:54:05 +0200

    Small Refactor. Load the toc extensions in a loop.

--- src/es/udc/cartolab/gvsig/tocextra/TocExtraExtension.java
+++ src/es/udc/cartolab/gvsig/tocextra/TocExtraExtension.java
@@ -19,6 +19,7 @@
 package es.udc.cartolab.gvsig.tocextra;
 
 import com.iver.andami.plugins.Extension;
+import com.iver.cit.gvsig.project.documents.contextMenu.AbstractContextMenuAction;
 import com.iver.utiles.extensionPoints.ExtensionPoints;
 import com.iver.utiles.extensionPoints.ExtensionPointsSingleton;
 
@@ -35,37 +36,29 @@ public class TocExtraExtension extends Extension {
 	}
 
 	public void initialize() {
-		ExtensionPoints extensionPoints = ExtensionPointsSingleton
-				.getInstance();
 
-		extensionPoints.add("View_TocActions", "ShowActives",
-				new ShowActivesTocMenuEntry());
-		extensionPoints.add("View_TocActions", "ShowOnlyActives",
-				new ShowOnlyActivesTocMenuEntry());
-		extensionPoints.add("View_TocActions", "ActivateVisibles",
-				new ActivateVisiblesTocMenuEntry());
+	AbstractContextMenuAction[] entries = new AbstractContextMenuAction[] {
+		new ShowActivesTocMenuEntry(),
+		new ShowOnlyActivesTocMenuEntry(),
+		new ActivateVisiblesTocMenuEntry(),
+		new InvertSelectionTocMenuEntry(), new ShowAllTocMenuEntry(),
+		new HideAllTocMenuEntry(), new ChangeNamesTocMenuEntry(),
+		new OpenAttributeTableTocMenuEntry() };
 
-		extensionPoints.add("View_TocActions", "InvertSelection",
-				new InvertSelectionTocMenuEntry());
-		extensionPoints.add("View_TocActions", "ShowAll",
-				new ShowAllTocMenuEntry());
-		extensionPoints.add("View_TocActions", "HideAll",
-				new HideAllTocMenuEntry());
-
-		extensionPoints.add("View_TocActions", "ChangeNames",
-				new ChangeNamesTocMenuEntry());
-		extensionPoints.add("View_TocActions", "ChangeName",
-				new ChangeName2TocMenuEntry());
-		
-		extensionPoints.add("View_TocActions", "OpenTable",
-				new OpenAttributeTableTocMenuEntry());
-
-		// Start and stop edition on several layers. It's too slow when there
-		// are many layers.
-		// extensionPoints.add("View_TocActions", "StartEditing", new
-		// StartEdition2TocMenuEntry());
-		// extensionPoints.add("View_TocActions", "ActivateEditing", new
-		// ActivateEditingTocMenuEntry());
+	ExtensionPoints extensionPoints = ExtensionPointsSingleton
+		.getInstance();
+	for (AbstractContextMenuAction entry : entries) {
+	    extensionPoints.add("View_TocActions", entry.getClass().getName(),
+		    entry);
+	}
+	extensionPoints.add("View_TocActions", "ChangeName",
+		new ChangeName2TocMenuEntry());
+	// Start and stop edition on several layers. It's too slow when there
+	// are many layers.
+	// extensionPoints.add("View_TocActions", "StartEditing", new
+	// StartEdition2TocMenuEntry());
+	// extensionPoints.add("View_TocActions", "ActivateEditing", new
+	// ActivateEditingTocMenuEntry());
 	}
 
 	public boolean isEnabled() {
